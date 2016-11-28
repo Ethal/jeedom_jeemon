807efd05d7508a57d5acc0025a5c544678d776a9
Cédric Dupont <lunarok@gmail.com>
2016-11-28 17:17:21 +0100

    error on log output

--- core/class/jeemon.class.php
+++ core/class/jeemon.class.php
@@ -135,10 +135,10 @@ class jeemon extends eqLogic {
                 } else {
                     $result = 0;
                 }
+                log::add('jeemon', 'debug', 'Cloud ' . $backup[0] . ' ' . date('Y-m-d', time() - 60 * 60 * 24) . ' ' . date('Y-m-d'));
             } else {
                 $result = 0;
             }
-            log::add('jeemon', 'debug', 'Cloud ' . $backup[0] . ' ' . date('Y-m-d', time() - 60 * 60 * 24) . ' ' . date('Y-m-d'));
             break;
             case 'logerr':
             $log_path = realpath(dirname(__FILE__) . '/../../../../log');
